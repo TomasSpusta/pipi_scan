a64fc1623b5e789b7f5dd6f4be7a47170bdf22ca
TomasSpusta <68905982+TomasSpusta@users.noreply.github.com>
2022-11-25 09:55:49 +0100

    Update network_check.py

--- network_check.py
+++ network_check.py
@@ -33,14 +33,7 @@ def network_check ():
             try:   
                 config.mac_address = gma() # get MAC address
                 print("My MAC adress is: {}".format(config.mac_address))
-                
-                try:
-                    # Send request to CRM to obtain equipment info according to MAC address
-                    web_requests.crm_request_mac()
-                except Exception as  e_CRM:
-                    print (e_CRM)
-                    print ("CRM has problem with MAC address")
-                    
+               
             except Exception as mac_e:
                 config.mac_address = " "
                 print (mac_e)
@@ -48,11 +41,7 @@ def network_check ():
         else:
             LCD_display.LCD_init (ip, config.mac_address)
             time.sleep (1)
-            
-
-    print ("Equipment name: " + str (config.equipment_name))
-    print ("Equipment ID: " + str (config.equipment_id))
-            
+           
     LCD_display.LCD_init (ip, config.mac_address)
 
     
